54cba0b9bf587c886218c8e651b7a245fbf60d36
Jiayih <angjy-pm17@student.tarc.edu.my>
2019-11-02 22:35:26 +0800

    constraint layout

--- app/src/main/java/com/example/practical22/MainActivity.kt
+++ app/src/main/java/com/example/practical22/MainActivity.kt
@@ -16,12 +16,18 @@ class MainActivity : AppCompatActivity() {
 
     private fun makeColored(view: View){
         when (view.id) {
-            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
-            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+
+            R.id.box_one_text ->  view.setBackgroundResource(R.drawable.download6)
+            R.id.box_two_text -> view.setBackgroundResource(R.drawable.download3)
             R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
             R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
             R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
-            else -> view.setBackgroundColor(Color.LTGRAY)
+            R.id.textView ->  view.setBackgroundResource(R.drawable.download2)
+            R.id.textView2 ->  view.setBackgroundResource(R.drawable.download1)
+            R.id.textView3 ->  view.setBackgroundResource(R.drawable.download5)
+            R.id.textView4 ->  view.setBackgroundResource(R.drawable.download)
+            else -> view.setBackgroundResource(R.drawable.download6)
+
         }
     }
 
@@ -31,11 +37,15 @@ class MainActivity : AppCompatActivity() {
         val boxThreeText = findViewById<TextView>(R.id.box_three_text)
         val boxFourText = findViewById<TextView>(R.id.box_four_text)
         val boxFiveText = findViewById<TextView>(R.id.box_five_text)
+        val textView = findViewById<TextView>(R.id.textView)
+        val textView2 = findViewById<TextView>(R.id.textView2)
+        val textView3 = findViewById<TextView>(R.id.textView3)
+        val textView4 = findViewById<TextView>(R.id.textView4)
 
         val rootConstraintLayout = findViewById<View>(R.id.constraint_layout)
 
         val clickableView: List<View> =
-            listOf(boxOneText,boxTwoText,boxThreeText,boxFourText,boxFiveText,rootConstraintLayout)
+            listOf(boxOneText,boxTwoText,boxThreeText,boxFourText,boxFiveText,textView,textView2,textView3,textView4,rootConstraintLayout)
 
         for(item in clickableView){
             item.setOnClickListener{makeColored(it)}
